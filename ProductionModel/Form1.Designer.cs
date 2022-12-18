890c8409e96fdfa7751d2ecc00d0c4c4d50c709e
Alina <ram@sfedu.ru>
2022-12-18 23:25:53 +0300

    done

---
+++ ProductionModel/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace ProductionModel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox4_rules = new System.Windows.Forms.ListBox();
+            this.list_facts_domainUpDown = new System.Windows.Forms.DomainUpDown();
+            this.label_res_reverse_output = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(241, 420);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Items.AddRange(new object[] {
+            "Выбранные факты:"});
+            this.listBox2.Location = new System.Drawing.Point(280, 69);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(241, 164);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            this.listBox2.DoubleClick += new System.EventHandler(this.listBox2_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Location = new System.Drawing.Point(280, 436);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(241, 58);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Прямой вывод";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Items.AddRange(new object[] {
+            "Результат прямого вывода:"});
+            this.listBox3.Location = new System.Drawing.Point(280, 239);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(241, 164);
+            this.listBox3.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button2.Location = new System.Drawing.Point(550, 289);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(241, 58);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Обратный вывод";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // clear
+            // 
+            this.clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.clear.Location = new System.Drawing.Point(558, 436);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(241, 58);
+            this.clear.TabIndex = 5;
+            this.clear.Text = "Очистить";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(280, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Тип вывода";
+            // 
+            // listBox4_rules
+            // 
+            this.listBox4_rules.FormattingEnabled = true;
+            this.listBox4_rules.ItemHeight = 16;
+            this.listBox4_rules.Items.AddRange(new object[] {
+            "Использованные правила:"});
+            this.listBox4_rules.Location = new System.Drawing.Point(550, 69);
+            this.listBox4_rules.Name = "listBox4_rules";
+            this.listBox4_rules.Size = new System.Drawing.Size(241, 164);
+            this.listBox4_rules.TabIndex = 12;
+            // 
+            // list_facts_domainUpDown
+            // 
+            this.list_facts_domainUpDown.Location = new System.Drawing.Point(550, 249);
+            this.list_facts_domainUpDown.Name = "list_facts_domainUpDown";
+            this.list_facts_domainUpDown.Size = new System.Drawing.Size(249, 22);
+            this.list_facts_domainUpDown.TabIndex = 13;
+            this.list_facts_domainUpDown.Text = "Проверить выводимость факта:";
+            // 
+            // label_res_reverse_output
+            // 
+            this.label_res_reverse_output.AutoSize = true;
+            this.label_res_reverse_output.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label_res_reverse_output.Location = new System.Drawing.Point(545, 363);
+            this.label_res_reverse_output.Name = "label_res_reverse_output";
+            this.label_res_reverse_output.Size = new System.Drawing.Size(230, 25);
+            this.label_res_reverse_output.TabIndex = 15;
+            this.label_res_reverse_output.Text = "Результат будет здесь";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(979, 517);
+            this.Controls.Add(this.label_res_reverse_output);
+            this.Controls.Add(this.list_facts_domainUpDown);
+            this.Controls.Add(this.listBox4_rules);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox4_rules;
+        private System.Windows.Forms.DomainUpDown list_facts_domainUpDown;
+        private System.Windows.Forms.Label label_res_reverse_output;
+    }
+}
+
---
+++ ProductionModel/Form1.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ProductionModel
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// facts selected by user
+        /// </summary>
+        List<string> using_facts = new List<string>();
+        /// <summary>
+        /// [name, id]
+        /// </summary>
+        Dictionary<string, string> facts = new Dictionary<string, string>();
+        /// <summary>
+        /// [id, name]
+        /// </summary>
+        Dictionary<string, string> inverseDic_facts;
+        
+        /// <summary>
+        /// rules[id] = [rule:THEN-part, IF-part]
+        /// </summary>
+        Dictionary<string, List<string>> rules = new Dictionary<string, List<string>>();
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1_facts_completion();
+            Rules_completion();
+        }
+
+        private void listBox1_facts_completion()
+        {
+            string file_name = "FactsForProductions.txt";
+            string lines = File.ReadAllText(file_name);
+
+            Regex regex_names = new Regex(@"'.+'");
+            MatchCollection matches_names;
+
+            Regex regex_ids = new Regex(@"[A-Z]\d+");
+            MatchCollection matches_ids;
+
+            matches_names = regex_names.Matches(lines);
+            matches_ids = regex_ids.Matches(lines);
+            
+            int i = 0;
+            foreach (Match match_id in matches_ids)
+            {
+                string id = match_id.Value;
+                string name = matches_names[i].Value;
+                //заполняем словарь фактов
+                facts[name] = id;
+                i++;
+
+                //заполняем окно фактов
+                listBox1.Items.Add(name);
+
+                //заполняем список фактов
+                list_facts_domainUpDown.Items.Add(name);
+            }
+
+            inverseDic_facts = facts.ToDictionary(g => g.Value,g => g.Key);
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            string fact = listBox1.SelectedItem.ToString();
+            string fact_id = facts[fact].ToString();
+            if (listBox2.Items.Contains(fact))
+            {
+                listBox2.Items.Remove(fact);
+                using_facts.Remove(fact_id);
+            }
+            else
+            {
+                listBox2.Items.Add(fact);
+                using_facts.Add(fact_id);
+            }
+        }
+
+        private void Rules_completion()
+        {
+            string file_name = "RulesForProductions.txt";
+            string lines = File.ReadAllText(file_name);
+
+            Regex regex_if = new Regex(@"[A-Z]\d+,[A-Z]\d+");
+            MatchCollection matches_if;
+
+            Regex regex_then = new Regex(@"- [A-Z]\d+");
+            MatchCollection matches_then;
+
+            Regex regex_id = new Regex(@"[A-Z]\d+ :");
+            MatchCollection matches_id;
+
+            matches_if = regex_if.Matches(lines);
+            matches_then = regex_then.Matches(lines);
+            matches_id = regex_id.Matches(lines);
+
+            int i = 0;
+            foreach (Match match_if in matches_if)
+            {
+                List<string> if_facts = match_if.Value.Split(',').ToList();
+                string then_fact = matches_then[i].Value.Substring(2);
+                string id = matches_id[i].Value.Substring(0, matches_id[i].Value.Length - 2);
+                //заполняем словарь правил
+                //rules[then_fact] = if_facts;
+
+                List<string> rules_facts = new List<string>();
+                rules_facts.Add(then_fact);
+                for(int j = 0; j < if_facts.Count; j++)
+                {
+                    rules_facts.Add(if_facts[j]);
+                }
+                rules[id] = rules_facts;
+
+                i++;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)//прямой вывод
+        {
+            label1.Text = "Прямой вывод";
+            listBox3.Items.Clear();
+            listBox4_rules.Items.Clear();
+
+            direct_output();
+        }
+
+        private void direct_output()
+        {
+            int depth = 0;
+            int k = -1;
+            foreach (var rule in rules)
+            {
+                k++;
+                List<string> cur_facts = new List<string>();
+                for(int i = 1; i< rule.Value.Count; i++)
+                {
+                    cur_facts.Add(rule.Value[i]);
+                }
+                
+                int count_match = 0;
+                for (int i = 0; i < cur_facts.Count; i++)
+                {
+                    for (int j = 0; j < using_facts.Count; j++)
+                    {
+                        if (using_facts[j] == cur_facts[i])
+                        {
+                            count_match++;
+                            if (count_match == cur_facts.Count)
+                            {
+                                using_facts.Add(rule.Value[0]);
+                                listBox3.Items.Add(inverseDic_facts[rule.Value[0]]);
+                                listbox4_rules_completion(k);
+                                depth++;
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        /*private void button2_Click(object sender, EventArgs e)//Обратный вывод
+        {
+            label1.Text = "Обратный вывод";
+            listBox3.Items.Clear();
+
+            int depth = 0;
+            bool change = false;
+            for(int k = 0; k < rules.Keys.Count; k++)
+            {
+                for(int i = 0; i < using_facts.Count; i++)
+                {
+                    string key_fact = rules.ElementAt(k).Key;
+                    if (using_facts[i] == key_fact)
+                    {
+                        change = true;
+                        using_facts.Remove(key_fact);
+                        List<string> purpose_facts = rules[key_fact];
+                        for (int j = 0; j < purpose_facts.Count; j++)
+                            if(!using_facts.Contains(purpose_facts[j]))
+                                using_facts.Add(purpose_facts[j]);
+                        k = 0;
+                        depth++;
+                    }
+                }
+            }
+            if(change)
+            {
+                for (int i = 0; i < using_facts.Count; i++)
+                    listBox3.Items.Add(inverseDic_facts[using_facts[i]]);
+            }
+        }*/
+
+        private void button2_Click(object sender, EventArgs e)//Обратный вывод
+        {
+            label1.Text = "Обратный вывод";
+            listBox3.Items.Clear();
+            listBox4_rules.Items.Clear();
+
+            for(int i = 0; i < using_facts.Count; i++)
+            {
+                TreeFactsNode fact = new TreeFactsNode(using_facts[i], new List<TreeFactsNode>());
+                Recurse_1(fact);
+            }
+
+            string id_fact = facts[list_facts_domainUpDown.Text.ToString()];
+            List<TreeFactsNode> children = new List<TreeFactsNode>();
+            TreeFactsNode root = new TreeFactsNode(id_fact, children);
+            
+            Recurse(root);
+
+            if (root.IsCover == true)
+                label_res_reverse_output.Text = "Из выбранных фактов вывод возможен";
+            else
+                label_res_reverse_output.Text = "Из выбранных фактов вывод невозможен";
+
+        }
+        private void Recurse_1(TreeFactsNode root)
+        {
+            for (int i = 0; i < rules.Keys.Count; i++)//идем по правилам
+            {
+                string key_fact = rules.ElementAt(i).Value[0];
+                if (key_fact == root.Value)
+                {
+                    listbox4_rules_completion(i);//добавляем в список используемых правил
+                    string rule_id = rules.ElementAt(i).Key;
+                    List<string> rule = rules.ElementAt(i).Value;
+                    List<string> children = new List<string>();
+                    for (int j = 1; j < rule.Count; j++)
+                    {
+                        children.Add(rule[j]);
+                    }
+                    for (int j = 0; j < children.Count; j++)
+                    {
+                        List<TreeFactsNode> empty_node_list = new List<TreeFactsNode>();
+                        TreeFactsNode c = new TreeFactsNode(root, children[j], empty_node_list);
+                        root.Children.Add(c);
+                        using_facts.Add(c.Value);
+                        Recurse_1(c);
+                    }
+                }
+            }
+        }
+        private void Recurse(TreeFactsNode root)
+        {
+            if (root == null) return;
+
+            for(int i = 0; i < using_facts.Count; i++)//отмечаем узлы-истинные_факты
+            {
+                if (root.Value == using_facts[i])
+                {
+                    root.IsCover = true;
+                    break;
+                }
+            }
+
+            for (int i = 0; i < rules.Keys.Count; i++)//идем по правилам
+            {
+                string key_fact = rules.ElementAt(i).Value[0];
+                if (key_fact == root.Value)
+                {
+                    string rule_id = rules.ElementAt(i).Key;
+                    List<string> rule = rules.ElementAt(i).Value;
+                    listbox4_rules_completion(i);//добавляем в список используемых правил
+                    List<string> children = new List<string>();
+                    for (int j = 1; j < rule.Count; j++)
+                    {
+                        children.Add(rule[j]);
+                    }
+                    for (int j = 0; j < children.Count; j++)
+                    {
+                        List<TreeFactsNode> empty_node_list = new List<TreeFactsNode>();
+                        TreeFactsNode c = new TreeFactsNode(root, children[j], empty_node_list);
+                        root.Children.Add(c);
+                        Recurse(c);
+                    }
+                    if (root.Children.Where(x => x.IsCover == true).Count() == 2)//если дочерние узлы отмечены, то отмечаем родителя
+                        root.IsCover = true;
+                    //break;
+                }
+            }
+        }
+
+        private void listbox4_rules_completion(int i)
+        {
+            string rule_id = rules.ElementAt(i).Key;
+            string rule = "";
+            for (int j = 1; j < rules[rule_id].Count; j++)
+            {
+                rule += inverseDic_facts[rules[rule_id][j]].ToString() + " ";
+                if (j != rules[rule_id].Count - 1)
+                    rule += "+ ";
+            }
+            rule += " = " + inverseDic_facts[rules[rule_id][0]].ToString();
+            listBox4_rules.Items.Add(rule);
+        }
+        private void clear_Click(object sender, EventArgs e)
+        {
+            using_facts.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+        }
+
+        private void listBox2_DoubleClick(object sender, EventArgs e)
+        {
+            string fact = listBox2.SelectedItem.ToString();
+            string fact_id = facts[fact].ToString();
+            listBox2.Items.Remove(fact);
+            using_facts.Remove(fact_id);
+            
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
